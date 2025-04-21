29a7384649ea3ef77ab4d2397a9637f22330a951
Tasheen2002 <darshikatasheen99@gmail.com>
2025-04-22 00:31:31 +0530

    Javascript added

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function checkVowels(){
-    const text=document.getElementById("inputText").value;
-    const vowelCount=0;
+    let text=document.getElementById("inputText").value;
+    let vowelCount=0;
 
-    text=text.toLowerCase();
+     text=text.toLowerCase();
 
     for(let i=0;i<text.length;i++){
-        let char=text.charA
+        let char=text.charAt(i);;
+        if(isVowel(char)){
+            vowelCount++;
+        }
     }
+
+    const result=document.getElementById("result");
+    result.textContent="Total vowels: "+vowelCount;
+}
+
+function isVowel(char){
+    const vowels=["a","e","i","o","u"];
+    return vowels.includes(char);
 }
